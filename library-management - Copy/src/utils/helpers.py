e57c1130d40db733653f21089d5c67362d09cbcd
Ramanpreet-max <ramanpreetkaurmalhi474@gmail.com>
2025-07-02 23:57:00 +0530

    Initial commit of portfolio

--- library-management - Copy/src/utils/helpers.py
+++
@@ -1,7 +0,0 @@
-def validate_isbn(isbn):
-    if len(isbn) == 10 or len(isbn) == 13:
-        return True
-    return False
-
-def format_book_details(book):
-    return f"Title: {book.title}, Author: {book.author}, ISBN: {book.isbn}"
